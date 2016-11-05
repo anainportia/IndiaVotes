ab593c56ec2e0e3bdd905aec2198923cb7bc9b4d
anainportia <anainportia@yopmail.com>
2016-11-05 13:09:40 +0530

    commiting first indiaVotes project

---
+++ PageObjects/Home.java
@@ -0,0 +1,33 @@
+package com.inportia;
+
+public class Home {
+	
+	String xpathTabHomeLink="//li/a[@title='Home']";
+	String xpathTabAboutusLink="//li/a[@title='About us']";
+	String xpathTabElectionMap="//li/a[@title='Election Maps']";
+	String xpathTabSummaryReport="//li/a[@title='Summary Report']";
+	String xpathTabBlog="//li/a[@title='Blog']";
+	String xpathTabBlog="//li/a[@title='Report Errors']";
+	String xpathTabRSSFEED=".//*[@id='main_menu']/div[1]/div[2]/a[1]/img";
+	String xpathTabFacebook=".//*[@id='main_menu']/div[1]/div[2]/a[2]/img";
+	String xpathTabtwitter=".//*[@id='main_menu']/div[1]/div[2]/a[3]/img";
+	
+	String CSSxpathDropDown="#state";
+	String CSSxpathDropDown="#stateac";
+	String CSSxpathDropDown="#yearac";
+	String CSSxpathDropDown="#partylist";
+	String CSSxpathDropDown="#statelist";
+	
+	String xpathTabPrtyButton=".//*[@id='form']/input[2]";
+	String xpathTabStateButton=".//*[@id='stateSummaryfrm']/input[2];"
+			
+	String xpathPCElectionGoButton=".//div[@id='error']/preceding-sibling::input";
+	String xpathPCElectionGoButton=".//select[@id='state']/following-sibling::input"
+			
+	String CSSXpathPCElectionGoButton=".searchIcon.fr";
+			
+	
+	
+	
+
+}
